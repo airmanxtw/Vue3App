6bbac5281c9cb965a1c8ae096757f6353000a117
airmanxtw <airmanx@stust.edu.tw>
2021-12-22 00:13:33 +0800

    test vue3

--- src/components/main.vue
+++ src/components/main.vue
@@ -2,10 +2,12 @@
   <div>
     <h1>hello vue3 {{ counter }}</h1>
     <button @click="add">click</button>
+    <my-title title="test title" :counter="counter" @save="add()"></my-title>
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
+import myTitle from "./myTitle.vue";
 const counter = ref(0);
 let add = () => {
   counter.value++;
---
+++ src/components/myTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ props.title }}-{{ props.counter }}-{{ counterLen }}
+    <button @click="divClick()">divClick</button>
+  </div>
+</template>
+<script setup>
+import { defineProps, computed, onMounted, defineEmits } from "vue";
+const props = defineProps({ title: String, counter: Number });
+const counterLen = computed(() => props.counter * 2);
+const emit = defineEmits(["save"]);
+const divClick = () => {
+  emit("save");
+};
+onMounted(() => {
+  //alert("hello");
+});
+</script>
